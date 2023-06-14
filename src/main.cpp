03aed3524c8684fb4f9b96184b0099fd25f52a03
aightech <long.devillard@gmail.com>
2023-06-14 13:27:12 +0100

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include "camera.hpp"
 #include "multi_camera.hpp"
-#include <com_client.hpp>
 #include <iostream>
+#include <lsl_c.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <lsl_c.h>
 
 using namespace cv;
 const int max_value_H = 360 / 2;
 const int max_value = 255;
-int low_H = 90, low_S = 171, low_V = 91;
-int high_H = 95, high_S = 222, high_V = 167;
-int dilate_size = 20;
+int low_H = 4, low_S = 92, low_V = 190;
+int high_H = 25, high_S = 195, high_V = 255;
+int dilate_size = 2;
 std::string win_name = "Win";
 
 static void
@@ -101,82 +100,59 @@ main(int argc, char **argv)
     params.minInertiaRatio = 0.01;
     detector = cv::SimpleBlobDetector::create(params);
 
-    MultiCam cameras;
-    int n = argc - 3;
+    if(argc < 2)
+    {
+        std::cout << "Usage: ./main <cam1_index> <cam2_index> ..." << std::endl;
+        return 0;
+    }
+
+    MultiCam cameras(2);
+    int n = argc - 1;
     std::cout << "Number of cameras: " << n << std::endl;
     int width = 3264;
     int height = 2448;
     double coef = 640. / width;
+    cv::Mat frame[n];
 
     for(int i = 0; i < n; i++)
-        cameras.addCamera(atoi(argv[i + 3]), width, height);
+        cameras.addCamera(atoi(argv[i + 1]), width, height);
 
-    /*** FISRT IMAGE RECORDING PHASE ****/
-    std::string img_name = argv[1];
-    int n_images = atoi(argv[2]);
-    std::cout << "Press any key to record images" << std::endl;
-
-    cv::Mat hdframe[2];
-    cv::Mat hdframe_resized[2];
-    cv::Mat frame[2];
-    for(int i = 0; i < n_images; i++)
+    for(int j = 0; j < n; j++)
     {
-        while(1)
-        {
-            for(int j = 0; j < 2; j++)
-            {
-                hdframe[j] = cameras.getFrame(j);
-                cv::resize(hdframe[j], hdframe_resized[j], cv::Size(0, 0), coef,
-                           coef);
-                cv::imshow("cam" + std::to_string(j), hdframe_resized[j]);
-            }
-            if(cv::waitKey(10) >= 0)
-                break;
-        }
-        for(int j = 0; j < 2; j++)
-            cv::imwrite(img_name + "_" + std::to_string(i) + "_c" +
-                            std::to_string(j) + ".png",
-                        hdframe[j]);
-        std::cout << "Image recorded! " << i + 1 << "/" << n_images
-                  << std::endl;
+        cameras.get_cam(j)->set_resolution(640, 480);
+        cameras.get_cam(j)->set_wrap("wrap"+std::to_string(j)+".xml");
     }
-    /*** -------------------------- ****/
-    for(int j = 0; j < 2; j++)
-      {
-      cv::resize(hdframe[j], hdframe_resized[j], cv::Size(0, 0), coef, coef);
-      cameras.m_cameras[j]->set_resolution(640, 480);
-      }
 
-    
-    int nb_ch = 4;
+    int nb_ch = 2*n;
     float data[nb_ch];
-    lsl_streaminfo info =
-      lsl_create_streaminfo("positions", "positions", nb_ch,
-                                  0, cft_float32, "camerasuid");
+    lsl_streaminfo info = lsl_create_streaminfo("positions", "positions", nb_ch,
+                                                0, cft_float32, "camerasuid");
     lsl_outlet outlet = lsl_create_outlet(info, 0, 360);
     for(;;)
     {
-      for(int j = 0; j < 2; j++)
-      {
-        frame[j] = cameras.getFrame(j);
-        std::vector<cv::KeyPoint> keypoints;
-        keypoints.push_back(cv::KeyPoint(-1, -1, 20));
-        get_keypoints(frame[j], keypoints);
+        std::cout << "\xd                                                                             \xd";
+        for(int j = 0; j < n; j++)
+        {
+            frame[j] = cameras.getFrame(j,true);
+            std::vector<cv::KeyPoint> keypoints;
+            keypoints.push_back(cv::KeyPoint(-1, -1, 20));
+            get_keypoints(frame[j], keypoints);
 
-        cv::Mat im_with_keypoints;
-        drawKeypoints(frame[j], keypoints, im_with_keypoints,
-                      cv::Scalar(0, 0, 255),
-                      cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-        cv::imshow("cam" + std::to_string(j), im_with_keypoints);
+            cv::Mat im_with_keypoints;
+            drawKeypoints(frame[j], keypoints, im_with_keypoints,
+                          cv::Scalar(0, 0, 255),
+                          cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+            cv::imshow("cam" + std::to_string(j), im_with_keypoints);
 
-        //send to client
-        data[j*2] = keypoints[0].pt.x/coef;
-        data[j*2+1] = keypoints[0].pt.y/coef;
-        std::cout << data[j*2] << "\t" << data[j*2+1] << "\t";
-      }
-      cv::imshow(win_name, inverted);
-      std::cout << "       \xd" <<std::flush;
-      lsl_push_sample_f(outlet, data);
+            //send to client
+            
+            data[j * 2] = keypoints[0].pt.x / frame[j].cols;
+            data[j * 2 + 1] = keypoints[0].pt.y / frame[j].rows;
+            std::cout << data[j * 2] << "\t\t" << data[j * 2 + 1] << "\t\t";
+        }
+        cv::imshow(win_name, inverted);
+        std::cout << std::flush;
+        lsl_push_sample_f(outlet, data);
 
         if(cv::waitKey(5) >= 0)
             break;
